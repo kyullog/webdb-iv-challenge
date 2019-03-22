eb6f85b7e4780e6c452899c18deada930a796d52
Jonathan Dorety <kyullog@gmail.com>
2019-03-21 22:43:05 -0400

    Make get dish by id function

--- data/dbHelper.js
+++ data/dbHelper.js
@@ -3,5 +3,11 @@ const db = require("./dbConfig.js");
 module.exports = {
   getDishes: () => {
     return db("dishes");
+  },
+
+  getDish: id => {
+    return db("dishes")
+      .where({ id })
+      .first();
   }
 };
--- index.js
+++ index.js
@@ -3,3 +3,7 @@ const db = require("./data/dbHelper.js");
 db.getDishes()
   .then(dishes => console.log(dishes))
   .catch(err => console.log(err));
+
+db.getDish(4)
+  .then(dish => console.log(dish))
+  .catch(err => console.log(err));
