be3babc4f159c38c6f71c51f298c9807f24db673
Jonathan Dorety <kyullog@gmail.com>
2019-03-21 21:40:44 -0400

    Create quantities table schema

---
+++ data/migrations/20190321213238_createQuantitiesTable.js
@@ -0,0 +1,38 @@
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable("quantities", tbl => {
+    tbl.increments();
+    tbl
+      .float("amount")
+      .unsigned()
+      .notNullable()
+      .unique();
+    tbl
+      .integer("unit_id")
+      .notNullable()
+      .unsigned()
+      .references("id")
+      .onTable("units")
+      .onDelete("CASCADE")
+      .onUpdate("CASCADE");
+    tbl
+      .integer("ingredient_id")
+      .notNullable()
+      .unsigned()
+      .references("id")
+      .inTable("ingredients")
+      .onDelete("CASCADE")
+      .onUpdate("CASCADE");
+    tbl
+      .integer("recipe_id")
+      .notNullable()
+      .unsigned()
+      .references("id")
+      .inTable("recipes")
+      .onDelete("CASCADE")
+      .onUpdate("CASCADE");
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists("quantities");
+};
