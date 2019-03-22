cb1d46fc4334e06efc9cd4c6381ca82abe862d35
Jonathan Dorety <kyullog@gmail.com>
2019-03-22 10:48:56 -0400

    Revise code to make DRY w/ John's suggestions

--- index.js
+++ index.js
@@ -1,25 +1,15 @@
 const db = require("./data/dbHelper.js");
 
-db.getDishes()
-  .then(dishes => console.log(dishes))
-  .catch(err => console.log(err));
-
-db.getDish(4)
-  .then(dish => console.log(dish))
-  .catch(err => console.log(err));
-
 const dish = { dish_name: "Fried Wombat" };
-
-db.addDish(dish)
-  .then(response => console.log(response))
-  .catch(err => console.log(err));
 
 const recipe = { recipe_name: "Spicy Wombat", dish_id: 10 };
 
-db.addRecipe(recipe)
-  .then(response => console.log(response))
-  .catch(err => console.log(err));
+const consoleLogger = response => console.log(response);
 
-db.getRecipes()
-  .then(recipes => console.log(recipes))
-  .catch(err => console.log(err));
+[
+  db.getDishes(),
+  db.getDish(4),
+  db.addDish(dish),
+  db.addRecipe(recipe),
+  db.getRecipes()
+].forEach(promise => promise.then(consoleLogger).catch(console.logger));
