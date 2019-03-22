0e7bdf43f174b038a7f2c7abc37a0a7a8bbfb2a4
Jonathan Dorety <kyullog@gmail.com>
2019-03-21 21:49:03 -0400

    Make seed data for dishes table

--- data/seeds/01-dishes.js
+++ data/seeds/01-dishes.js
@@ -1,13 +1,17 @@
-
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
+  return knex("dishes")
+    .truncate()
+    .then(function() {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex("dishes").insert([
+        { dish_name: "Yummy Tacos" },
+        { dish_name: "Supreme Pizza" },
+        { dish_name: "Scrambled Eggs" },
+        { dish_name: "S&P Catfish" },
+        { dish_name: "Hot Wings" },
+        { dish_name: "Ham and Cheese Sandwich" },
+        { dish_name: "Grilled Cheese Sandwich" }
       ]);
     });
 };
