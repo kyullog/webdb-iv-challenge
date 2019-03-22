39b5bd92f4124c1e64903051b7aa2f7bb6533858
Jonathan Dorety <kyullog@gmail.com>
2019-03-21 21:43:08 -0400

    Fix typo

--- data/migrations/20190321213238_createQuantitiesTable.js
+++ data/migrations/20190321213238_createQuantitiesTable.js
@@ -11,7 +11,7 @@ exports.up = function(knex, Promise) {
       .notNullable()
       .unsigned()
       .references("id")
-      .onTable("units")
+      .inTable("units")
       .onDelete("CASCADE")
       .onUpdate("CASCADE");
     tbl
