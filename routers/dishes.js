d01bc691dba8fc87eb8c7036944147a0226f328c
Jonathan Dorety <kyullog@gmail.com>
2019-03-22 14:46:30 -0400

    Implement get dish by id API endpoint

--- routers/dishes.js
+++ routers/dishes.js
@@ -13,4 +13,18 @@ router.get("/", async (req, res) => {
   }
 });
 
+router.get("/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const dish = await db.getDish(id);
+    if (dish) {
+      res.status(200).json(dish);
+    } else {
+      res.status(404).json({ error: "No dish exists with that ID" });
+    }
+  } catch (err) {
+    res.status(500).json({ error: "There was a problem retrieving the dish" });
+  }
+});
+
 module.exports = router;
