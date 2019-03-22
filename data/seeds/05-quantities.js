d8ec35ca0787cf643c2244c34630ac35eb4be26f
Jonathan Dorety <kyullog@gmail.com>
2019-03-21 22:21:24 -0400

    Make seed data for quantities table

--- data/seeds/05-quantities.js
+++ data/seeds/05-quantities.js
@@ -1,13 +1,29 @@
-
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
+  return knex("quantities")
+    .truncate()
+    .then(function() {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex("quantities").insert([
+        { amount: 3, unit_id: 8, ingredient_id: 1, recipe_id: 1 },
+        { amount: 1, unit_id: 3, ingredient_id: 3, recipe_id: 1 },
+        { amount: 0.75, unit_id: 3, ingredient_id: 21, recipe_id: 1 },
+        { amount: 3, unit_id: 8, ingredient_id: 2, recipe_id: 2 },
+        { amount: 1, unit_id: 3, ingredient_id: 3, recipe_id: 2 },
+        { amount: 0.75, unit_id: 3, ingredient_id: 21, recipe_id: 2 },
+        { amount: 2, unit_id: 8, ingredient_id: 4, recipe_id: 3 },
+        { amount: 2, unit_id: 8, ingredient_id: 4, recipe_id: 4 },
+        { amount: 2, unit_id: 8, ingredient_id: 4, recipe_id: 5 },
+        { amount: 1, unit_id: 1, ingredient_id: 5, recipe_id: 4 },
+        { amount: 0.65, unit_id: 7, ingredient_id: 9, recipe_id: 6 },
+        { amount: 0.65, unit_id: 7, ingredient_id: 9, recipe_id: 7 },
+        { amount: 2, unit_id: 3, ingredient_id: 17, recipe_id: 6 },
+        { amount: 1.5, unit_id: 1, ingredient_id: 6, recipe_id: 7 },
+        { amount: 1, unit_id: 2, ingredient_id: 18, recipe_id: 6 },
+        { amount: 1, unit_id: 2, ingredient_id: 19, recipe_id: 6 },
+        { amount: 10, unit_id: 8, ingredient_id: 25, recipe_id: 9 },
+        { amount: 2, unit_id: 3, ingredient_id: 17, recipe_id: 9 },
+        { amount: 1, unit_id: 3, ingredient_id: 26, recipe_id: 9 }
       ]);
     });
 };
